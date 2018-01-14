bcb723c27a84f7577a5f1d4735c8122dd17c7240
spareribs <zhangjintian0118@gmail.com>
2018-01-14 17:41:52 +0800

    usr meta to save front_images_url

--- SpareribsSpider/SpareribsSpider/spiders/jobbole.py
+++ SpareribsSpider/SpareribsSpider/spiders/jobbole.py
@@ -20,15 +20,23 @@ class JobboleSpider(scrapy.Spider):
         2. 获取下一页URL并交给Scrapy进行下载，下载完成后交给parse
         """
         # 解析列表页面所有文章URL并交给Scrapy下载后进行解析
-        post_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
-        for post_url in post_urls:
-            print(post_url)
-            yield Request(url=urlparse.urljoin(response.url, post_url), callback=self.parse_detail)
+        post_nodes = response.css("#archive .floated-thumb .post-thumb a")
+        for post_node in post_nodes:
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            post_url = post_node.css("::attr(href)").extract_first("")
+            yield Request(
+                url=urlparse.urljoin(response.url, post_url),
+                meta={"front_images_url": urlparse.urljoin(response.url, image_url)},
+                callback=self.parse_detail
+            )
 
         # 提取下一页并交给Scrapy进行下载
-        next_url = response.css(".next.page-numbers::attr(href)").extract_first()
+        next_url = response.css(".next.page-numbers::attr(href)").extract_first("")
         if next_url:
-            yield Request(url=urlparse.urljoin(response.url, next_url), callback=self.parse)
+            yield Request(
+                url=urlparse.urljoin(response.url, next_url),
+                callback=self.parse
+            )
 
     def parse_detail(self, response):
         """
@@ -69,26 +77,27 @@ class JobboleSpider(scrapy.Spider):
         # ******************
         # use css selector *
         # ******************
-        title = response.css(".entry-header h1::text").extract_first()
-        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract_first().split()[0]
-        prasise_nums = response.css("span.vote-post-up h10::text").extract_first()
-        fav_nums = response.css("span.bookmark-btn::text").extract_first()
+        front_images_url = response.meta.get("front_images_url", "")
+        title = response.css(".entry-header h1::text").extract_first("")
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract_first("").split()[0]
+        prasise_nums = response.css("span.vote-post-up h10::text").extract_first("")
+        fav_nums = response.css("span.bookmark-btn::text").extract_first("")
         match_re = re.match(".*?(\d+).*", fav_nums)
         if match_re:
             fav_nums = int(match_re.group(1))
         else:
             fav_nums = 0
 
-        commonts_nums = response.css("a[href*='#article-comment'] span::text").extract_first()
+        commonts_nums = response.css("a[href*='#article-comment'] span::text").extract_first("")
         match_re = re.match(".*?(\d+).*", commonts_nums)
         if match_re:
             commonts_nums = int(match_re.group(1))
         else:
             fav_nums = 0
 
-        content = response.css("div.entry").extract_first()
+        content = response.css("div.entry").extract_first("")
 
-        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract_first()
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract_first("")
 
         suffix = "评论"
         tags = []
