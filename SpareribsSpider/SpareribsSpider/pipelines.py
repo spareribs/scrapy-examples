b60db9ac47fcee02889ec9e40cfe2cdfdb984571
Spareribs <370835062@qq.com>
2018-01-16 13:47:32 +0800

    自定义JSON pipline 保存数据

--- SpareribsSpider/SpareribsSpider/pipelines.py
+++ SpareribsSpider/SpareribsSpider/pipelines.py
@@ -5,12 +5,29 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import codecs
+import json
 from scrapy.pipelines.images import ImagesPipeline
 
 
 class SpareribsspiderPipeline(object):
     def process_item(self, item, spider):
         return item
+
+
+class JsonWithEncodingPipeline(object):
+    # 自定义json文件的导出
+    def __init__(self):
+        self.file = codecs.open('article.json', 'w', encoding="utf-8")
+
+    def process_item(self, item, spider):
+        lines = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(lines)
+        return item
+
+    def spider_closed(self, spider):
+        """当Spider关闭的时候，会顺便关闭文件"""
+        self.file.close()
 
 
 class ArticleImagesPipeline(ImagesPipeline):
--- SpareribsSpider/SpareribsSpider/settings.py
+++ SpareribsSpider/SpareribsSpider/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = False
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'SpareribsSpider.pipelines.SpareribsspiderPipeline': 300,
+    'SpareribsSpider.pipelines.JsonWithEncodingPipeline': 2,
     # 'scrapy.pipelines.images.ImagesPipeline': 1,
     'SpareribsSpider.pipelines.ArticleImagesPipeline': 1
 }
