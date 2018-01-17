6b294c5d71f01c3985c60e8c8946dcc72f9dc87e
Spareribs <370835062@qq.com>
2018-01-17 13:40:04 +0800

    使用scrapy item loader机制 解释response中的数据

--- SpareribsSpider/SpareribsSpider/spiders/jobbole.py
+++ SpareribsSpider/SpareribsSpider/spiders/jobbole.py
@@ -3,6 +3,7 @@ import re
 import scrapy
 import datetime
 from scrapy.http import Request
+from scrapy.loader import ItemLoader
 from SpareribsSpider.items import JobBoleAticleItem
 from SpareribsSpider.utils.common import get_md5
 
@@ -128,4 +129,26 @@ class JobboleSpider(scrapy.Spider):
         article_item["fav_nums"] = fav_nums
         article_item["comment_nums"] = comment_nums
 
+        # 通过item loader加载item
+        """
+        1. item_loader常用的函数
+        item_loader.add_css()
+        item_loader.add_xpath()
+        item_loader.add_value()
+        2. 默认取值都是List对象
+        """
+        front_image_url = response.meta.get("front_image_url", "")  # 文章封面图
+        item_loader = ItemLoader(item=JobBoleAticleItem(), response=response)
+        item_loader.add_css("title", ".entry-header h1::text")
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("url_object_id", get_md5(response.url))
+        item_loader.add_css("create_date", "p.entry-meta-hide-on-mobile::text")
+        item_loader.add_value("front_image_url", [front_image_url])
+        item_loader.add_css("praise_nums", ".vote-post-up h10::text")
+        item_loader.add_css("comment_nums", "a[href='#article-comment'] span::text")
+        item_loader.add_css("fav_nums", ".bookmark-btn::text")
+        item_loader.add_css("tags", "p.entry-meta-hide-on-mobile a::text")
+        item_loader.add_css("content", "div.entry")
+
+        article_item = item_loader.load_item()
         yield article_item
